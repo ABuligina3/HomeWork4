958ccbf7166c604f22ccd6f29fb27b4c32936fe9
ABulygina <olgabulygina72@mail.ru>
2023-05-17 13:44:26 +0300

    Homework5.3

---
+++ src/HashSetBucket.java
@@ -0,0 +1,28 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
+import static java.util.Objects.isNull;
+
+public class HashSetBucket {
+    private List<ValueEntry> entries;
+
+    public HashSetBucket() {
+        if(isNull(entries)) {
+            entries = new LinkedList<>();
+        }
+    }
+
+    public List<ValueEntry> getEntries() {
+        return entries;
+    }
+
+    public void addEntry(ValueEntry entry) {
+        this.entries.add(entry);
+    }
+
+    public void removeEntry(ValueEntry entry) {
+        this.entries.remove(entry);
+    }
+}
+
---
+++ src/ValueEntry.java
@@ -0,0 +1,17 @@
+public class ValueEntry<V> {
+
+
+    private V value;
+
+    public ValueEntry(V value) {
+        this.value = value;
+    }
+
+    public V getValue() {
+        return value;
+    }
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+}
